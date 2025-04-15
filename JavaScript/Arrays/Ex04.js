21aefe1adc10ba414ae1e6b619f6ec8891fc8131
Bryan Inacio <bryan.inacio@aluno.senai.br>
2025-04-15 09:32:56 -0300

    feat: Iniciando os Desafios

--- JavaScript/Arrays/Ex04.js
+++ JavaScript/Arrays/Ex04.js
@@ -4,4 +4,20 @@ var prompt = require('prompt-sync')();
 
 let AlunosPresentes = [];
 
+let Presenca = [];
+
 let quantidade = parseInt(prompt("Quantos Alunos a Sala Possui? "));
+
+console.log();
+
+for (let i = 0; i < quantidade; i++) {
+    Presenca = prompt("Nome do Aluno: ");
+    AlunosPresentes.push(Presenca);
+}
+
+console.log();
+
+console.log("Alunos Presentes:");
+for (let i = 0; i < AlunosPresentes.length;i++){
+    console.log("- " + AlunosPresentes[i]);
+}
